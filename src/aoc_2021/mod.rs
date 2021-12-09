3f39e5cee26dd5c8b2a63511ae686a7eb24cfe97
Daniel Forde <daniel.forde001@gmail.com>
2021-12-09 20:03:00 +0200

    Solution for day 8.

--- src/aoc_2021/mod.rs
+++ src/aoc_2021/mod.rs
@@ -1,14 +1,23 @@
 mod day_1_part_1;
 mod day_1_part_2;
+
 mod day_2_part_1;
 mod day_2_part_2;
+
 mod day_3_part_1;
 mod day_3_part_2;
+
 mod day_4_part_1;
 mod day_4_part_2;
+
 mod day_5_part_1;
 mod day_5_part_2;
+
 mod day_6_part_1;
 mod day_6_part_2;
+
 mod day_7_part_1;
 mod day_7_part_2;
+
+mod day_8_part_1;
+mod day_8_part_2;
